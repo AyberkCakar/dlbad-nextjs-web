8f5abe48043836cd6f591130f68fe2acffbc168f
Ayberk Cakar <ayberkcakar35@gmail.com>
2023-10-08 22:43:26 +0300

    fix: fixed layout component

--- components/layout/_style.tsx
+++ components/layout/_style.tsx
@@ -1,12 +1,13 @@
-import styled from "styled-components";
-import { createGlobalStyle } from "styled-components";
-import { IGlobalStyle } from "./_type";
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import { IGlobalStyle } from './_type';
 
 export const GlobalStyle = createGlobalStyle<IGlobalStyle>`
   body {
     background-color: ${(props) => props.backgroundColor};
     margin: 0;
     padding: 0;
+    font-family: "Roboto","Helvetica","Arial",sans-serif;
   }
 `;
 
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-import { GlobalStyle, LayoutBase } from "./_style";
-import { useTheme } from "../../hooks/useTheme";
+import React from 'react';
+import { GlobalStyle, LayoutBase } from './_style';
+import { useTheme } from '../../hooks/useTheme';
+import { Footer } from '../footer';
+import { Header } from '../header';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const { theme } = useTheme();
 
   return (
     <LayoutBase>
+      <Header />
       <GlobalStyle
         backgroundColor={theme.colors.primaryBackgroundColor as string}
       />
       {children}
+      <Footer />
     </LayoutBase>
   );
 };

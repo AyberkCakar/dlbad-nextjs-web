36c0db3fd7bf2d6cf37ab8df5a2c778ecb2fe8f2
Ayberk Cakar <ayberkcakar35@gmail.com>
2023-10-08 22:44:47 +0300

    feat: added simulator page

---
+++ pages/simulator.tsx
@@ -0,0 +1,6 @@
+import type { NextPage } from 'next';
+import SimulatorPage from '../components/pages/simulator/simulator.component';
+
+const Simulator: NextPage = () => <SimulatorPage />;
+
+export default Simulator;

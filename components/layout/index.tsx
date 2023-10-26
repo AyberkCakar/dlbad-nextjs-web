866aa0640bec9970aff41212cac11b4d161ae4f2
Ayberk Cakar <ayberkcakar35@gmail.com>
2023-10-26 23:47:55 +0300

    feat: added authentication structure

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GlobalStyle, LayoutBase } from './_style';
 import { useTheme } from '../../hooks/useTheme';
 import { Footer } from '../footer';
 import { Header } from '../header';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { useRouter } from 'next/router';
+import { UserService } from '../../utils/services/userService';
+import { verifyJwtToken } from '../../lib/verifyJwt';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const { theme } = useTheme();
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const router = useRouter();
 
-  return (
+  React.useEffect(() => {
+    const user = UserService.getUser();
+    if (user) {
+      verifyJwtToken(user.token)
+        .then((isVerify) => {
+          setIsLogin(isVerify);
+
+          if (!isVerify) {
+            router.replace('/login');
+          }
+        })
+        .catch((error) => {
+          setIsLogin(false);
+          router.replace('/login');
+        });
+    }
+  }, [router]);
+
+  return isLogin ? (
     <LayoutBase>
       <Header />
       <GlobalStyle
@@ -17,6 +40,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       {children}
       <Footer />
     </LayoutBase>
+  ) : (
+    <LayoutBase>{children}</LayoutBase>
   );
 };
 
---
+++ utils/services/routers.tsx
@@ -0,0 +1,7 @@
+export const protectedRoutes = [
+  '/',
+  '/simulator',
+  '/add-simulator',
+  '/failure-types'
+];
+export const authRoutes = ['/login', '/sign-up'];

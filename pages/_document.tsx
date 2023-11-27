d5d9f0ca98d5c3613c34472aa3150fff905107c1
AyberkCakar <ayberkcakar35@gmail.com>
2023-11-27 21:37:46 +0300

    feat: added mui ui ssr settings

---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document';
+import { ServerStyleSheets } from '@mui/styles';
+import React from 'react';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />)
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: [
+        React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement()
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
